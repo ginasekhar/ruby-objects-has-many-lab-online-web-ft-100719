17ec948e01daf8053b9c974fa7a11b3a61d03e2c
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-17 22:08:13 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,6 +2,7 @@ class Artist
   attr_accessor :name
   
   @songs = []
+  @@songs_to_artists_count = 0
  
   def initialize(name)
     @name = name
@@ -9,13 +10,19 @@ class Artist
  
   def add_song(song)
     song.artist = self
+    @@songs_to_artists_count +=1 
   end
   
   def add_song_by_name(name)
     song = Song.new(name)
     song.artist = self
+    @@songs_to_artists_count +=1 
   end
  
+  def self.song_count
+    @@all.size 
+  end
+  
   def songs
     Song.all.select {|song| song.artist == self}
   end
